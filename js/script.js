f0a974f5233902eef32fe5b8c1851be29253bd03
JOSE <jose@Julianas-MacBook-Air.local>
2023-11-09 18:00:49 -0500

    trabajo terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
  // Aquí tu código
+
+ 
+ 
+ const button = document.getElementById('agregar');
+
+ const lista = document.getElementById('lista');
+
+
+
+
+ button.addEventListener('click', function () {
+    const feedback = prompt('Introduce los ingredientes aquí');
+    const nuevoElemento = document.createElement('li');
+    nuevoElemento.innerHTML = feedback;
+    lista.appendChild(nuevoElemento);
+ });
